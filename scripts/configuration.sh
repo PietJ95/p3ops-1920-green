c2355efc9662f5687d971181a99e66ec509c3a51
Joeri Verhavert <joeri.verhavert.y7236@student.hogent.be>
2019-11-04 15:09:57 +0100

    Script aangepast

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
-#Add sudo user admin
-echo "Adding sudo user, named admin.."
-useradd admin -m -p "AdminGreen1"
-usermod -aG wheel admin
-
 #Changing user
-echo "loggin in with admin.."
-su admin
+echo "loggin in with student sudo rights  .."
 sudo su -
-
+echo "-----------------------------"
 #Installing kvm and starting kvm
-echo "Installing kvm.."
+echo "-----------------------------"
+echo "Installing and starting kvm.."
 yum install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install -y
-echo "Starting kvm.."
 service enable libvirtd
 service start libvirtd
-
+echo "-----------------------------"
 #Installing vagrant
 echo "Installing and configure vagrant.."
 yum -y install https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_x86_64.rpm
-
+echo "-----------------------------"
 #Installing git
 echo "Installing git on system.."
-yum install git -y 
-
+yum install git -y
+echo "-----------------------------"
 #Installing and enable ssh
 yum install -y openssh-server
 service sshd start
-
+echo "-----------------------------"
 #Cloning the project repo
 cd /home/admin/
 git clone https://github.com/HoGentTIN/p3ops-1920-green.git
-
-# Updating Centos machine. 
+echo "-----------------------------"
+# Updating Centos machine.
 echo "Updating system.."
 yum -y update
-
+echo "-----------------------------"
 #Prompt for clearing
 Read -p "Configuration has completed, type 1 to continue.." input
 if $input=1
 	then clear
 fi
 
-#Showing ip 
-ip ad
+#Showing ip
+echo "-----------------------------"
+#$ /sbin/ifconfig eth0| grep 'inet addr:'
+#$ /sbin/ifconfig eth1| grep 'inet addr:'
+#$ /sbin/ifconfig lo| grep 'inet addr:'
+echo "Ip wordt hier normaal getoond, nog te doen"
+
+echo "-----------------------------"
 
 #Prompt for reboot
 Read -p "Type 1 to reboot system.." input
@@ -52,4 +52,3 @@ if $input=1
 	echo "Rebooting sytem.."
 	reboot -f
 fi
-
